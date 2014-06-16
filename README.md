b46c700a751604ebe2caff64f28af8d429748d92
Tom Anderson <dante.renevo@gmail.com>
2014-06-16 12:02:46 -0700

    Setting up gradle and the mod.

--- README.md
+++ README.md
@@ -2,4 +2,11 @@ A collection of tweaks (with config) used on the Burpcraft Server.
 
 [WTFPL License](http://www.wtfpl.net)
 
+Working with the code
+
+    gradlew setupDevWorkspace
+    gradlew eclipse
+    gradlew build
+
+
 
---
+++ src/main/java/com/renevo/burptweaks/BurpTweaksMod.java
@@ -0,0 +1,19 @@
+package com.renevo.burptweaks;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = BurpTweaksMod.MODID, version = BurpTweaksMod.VERSION)
+public class BurpTweaksMod
+{
+    public static final String MODID = "burptweaks";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
